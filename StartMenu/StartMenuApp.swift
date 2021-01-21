3c03baca23d41fcb63965be2d80db971b8729e0b
denvolkov <denvolkov@inbox.ru>
2021-01-21 09:52:21 +0300

    Initial Commit

---
+++ StartMenu/StartMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartMenuApp.swift
+//  StartMenu
+//
+//  Created by test on 21.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct StartMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
